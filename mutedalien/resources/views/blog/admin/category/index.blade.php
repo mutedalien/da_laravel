fe51ef42b6b8a47db4d86cab5272098dd7da7dbc
Svyatoslav <chel_c@mail.ru>
2020-09-10 20:06:15 +0300

    less_15

--- mutedalien/resources/views/blog/admin/category/index.blade.php
+++ mutedalien/resources/views/blog/admin/category/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12">
@@ -25,7 +26,7 @@
                                     <td>
                                         <a href="{{ route('blog.admin.categories.edit', $item->id) }}"></a>
                                     </td>
-                                    <td @if(in_array($item->parent_id, [0, 1])) style="" @endif>
+                                    <td @if(in_array($item->parent_id, [0, 1])) style="color: #ccc" @endif>
                                         {{ $item->parent_id }}{{-- $item->parentCategory->tytle --}}
                                     </td>
                                 </tr>
@@ -36,4 +37,17 @@
                 </div>
             </div>
         </div>
+        @if($paginator->total() > $paginator->count())
+            <br>
+            <div class="row justify-content-center">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-body">
+                            {{ $paginator->links() }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
     </div>
+@endsection
