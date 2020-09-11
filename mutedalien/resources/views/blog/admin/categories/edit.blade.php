4c0c47201439cf50e8e8e0de09598e5f7ec84b03
Svyatoslav <chel_c@mail.ru>
2020-09-11 19:49:05 +0300

    less_18

--- mutedalien/resources/views/blog/admin/categories/edit.blade.php
+++ mutedalien/resources/views/blog/admin/categories/edit.blade.php
@@ -6,12 +6,18 @@
         @method('PATCH')
         @csrf
         <div class="container">
+            @php
+            /** @var \Illuminate\Support\ViewErrorBag $errors */
+            @endphp
+            @if($errors->any()) @endif
+
+            @if(session('success')) @endif
             <div class="row justify-content-center">
                 <div class="col-md-8">
-                    @include('blog.admin.category.includes.item_edit_main_col')
+                    @include('blog.admin.categories.includes.item_edit_main_col')
                 </div>
                 <div class="col-md-3">
-                    @include('blog.admin.category.includes.item_edit_add_col')
+                    @include('blog.admin.categories.includes.item_edit_add_col')
                 </div>
             </div>
         </div>
