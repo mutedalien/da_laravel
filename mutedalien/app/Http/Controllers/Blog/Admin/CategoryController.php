cfe9cb03e3d2863b2a495332201b7ded2681406b
Svyatoslav <chel_c@mail.ru>
2020-09-14 20:38:27 +0300

    less_28

--- mutedalien/app/Http/Controllers/Blog/Admin/CategoryController.php
+++ mutedalien/app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Blog\Admin;
 use App\Http\Requests\BlogCategoryCreateRequest;
 use App\Http\Requests\BlogCategoryUpdateRequest;
 use App\Models\BlogCategory;
-use Illuminate\Http\Request;
+use App\Repositories\BlogCategoryRepository;
 
 class CategoryController extends BaseController
 {
@@ -74,10 +74,13 @@ class CategoryController extends BaseController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($id, BlogCategoryRepository $categoryRepository)
     {
-        $item = BlogCategory::findOrFail($id);
-        $categoryList = BlogCategory::all();
+        //$item = BlogCategory::findOrFail($id);
+        //$categoryList = BlogCategory::all();
+
+        $item = $categoryRepository->getEdit($id);
+        $categoryList = $categoryRepository->getForComboBox();
 
         return view('blog.admin.categories.edit',
         compact('item', 'categoryList'));
---
+++ mutedalien/app/Repositories/BlogCategoryRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BlogCategory as Model;
+use Illuminate\Database\Eloquent\Collection;
+
+/**
+ * Class BlogCategoryRepository
+ *
+ * @package App\Repositories
+ */
+class BlogCategoryRepository extends CoreRepository
+{
+    /**
+     * Получить модель для редактирования в админке.
+     *
+     * @param int $id
+     *
+     * @return Model
+     */
+    public function getEdit($id)
+    {
+        return $this->startConditions()-find($id);
+    }
+
+    /**
+     * Получить список категорий для вывода в выпадающем списке.
+     *
+     * @return Collection
+     */
+    public function getModelClass()
+    {
+        return Model::class;
+    }
+}
