352b36847244486a873d13c85279ef84bd795656
Svyatoslav <chel_c@mail.ru>
2020-09-18 21:04:12 +0300

    less_41 php artisan make:observer BlogPostObserver --model=Models\BlogPost php artisan make:observer BlogCategoryObserver --model=Models\BlogCategory

---
+++ mutedalien/app/Observers/BlogPostObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\BlogPost;
+
+class BlogPostObserver
+{
+    /**
+     * Handle the blog post "created" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function created(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the blog post "updated" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function updated(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the blog post "deleted" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function deleted(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the blog post "restored" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function restored(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the blog post "force deleted" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function forceDeleted(BlogPost $blogPost)
+    {
+        //
+    }
+}
