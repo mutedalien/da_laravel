6537bc951a6d68d058017c7c09bf48bb23150364
Svyatoslav <chel_c@mail.ru>
2020-09-22 22:40:44 +0300

    less_46

--- mutedalien/app/Models/BlogCategory.php
+++ mutedalien/app/Models/BlogCategory.php
@@ -66,25 +66,25 @@ class BlogCategory extends Model
         return $this->id == BlogCategory::ROOT;
     }
 
-//    /**
-//     * Пример аксесуара
-//     *
-//     * @param string $valueFormDB
-//     *
-//     * @return bool|mixed|null|string|string[]
-//     */
-//    public function getTitleAttribute($valueFormObject)
-//    {
-//        return mb_strtoupper($valueFormObject);
-//    }
-//
-//    /**
-//     * Пример мутатора
-//     *
-//     * @param string $incomingValue
-//     */
-//    public function setTitleAttribute($incomingValue)
-//    {
-//        $this->attributes['title'] = mb_strtolower($incomingValue);
-//    }
+    /**
+     * Пример аксесуара
+     *
+     * @param string $valueFormDB
+     *
+     * @return bool|mixed|null|string|string[]
+     */
+    public function getTitleAttribute($valueFormObject)
+    {
+        return mb_strtoupper($valueFormObject);
+    }
+
+    /**
+     * Пример мутатора
+     *
+     * @param string $incomingValue
+     */
+    public function setTitleAttribute($incomingValue)
+    {
+        $this->attributes['title'] = mb_strtolower($incomingValue);
+    }
 }
