c85bdd7f42f3480a103cc790ee72d3807aa6607c
Svyatoslav <chel_c@mail.ru>
2020-09-27 22:08:26 +0300

    less_48

--- mutedalien/app/Http/Controllers/DiggingDeeperController.php
+++ mutedalien/app/Http/Controllers/DiggingDeeperController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class DiggingDeeperController extends Controller
 {
-    //
+    /**
+     * Базовая информация
+     * @url https://laravel.com/docs/5.8/collections
+     *
+     * справочная информация:
+     * https://laravel.com/api/5.8/Illuminate/Support/Collection.html
+     *
+     * вариант коллекции для моделей eloquent:
+     * https://laravel.com/api/5.8/Illuminate/Database/Eloquent/Collection.html
+     *
+     * Билдер запросов - то, с чем можно перепутать коллекции:
+     * https://laravel.com/docs/5.8/queries
+     */
+    public function collections() {
+        
+    }
 }
